d3fdec7f0f5bd947b3d9a1b048223a69863acf1d
Frank Osorio <frankosorio4@gmail.com>
2024-11-11 20:58:03 -0300

    feat: update, separating Method POST cadastrar from other usuarios methods

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -4,120 +4,119 @@ const padraoCPF = new RegExp(/^\d{11}$/);
 const padraoCEP = new RegExp(/^\d{8}$/);
 const Usuario = require("../models/Usuario");
 const Local = require("../models/Local");
-const Atividade = require("../models/Atividade");
 
 class UsuarioController {
-    async cadastrarUsuario(request, response) {
-        try {
-            const dados = request.body;
+    // async cadastrarUsuario(request, response) {
+    //     try {
+    //         const dados = request.body;
 
-            if (!dados.nome || !dados.sexo || !dados.cpf || !dados.dataNascimento || !dados.email || !dados.password || !dados.cep || !dados.logradouro || !dados.bairro || !dados.cidade || !dados.estado) {
-                return response.
-                    status(400).
-                    json({ mensagem: 'Um o mais dados faltantes. O nome, sexo, cpf, email, password, cep, logradouro, bairro, cidade, estado e data de nascimento são obrigatórios.' })
-            }
+    //         if (!dados.nome || !dados.sexo || !dados.cpf || !dados.dataNascimento || !dados.email || !dados.password || !dados.cep || !dados.logradouro || !dados.bairro || !dados.cidade || !dados.estado) {
+    //             return response.
+    //                 status(400).
+    //                 json({ mensagem: 'Um o mais dados faltantes. O nome, sexo, cpf, email, password, cep, logradouro, bairro, cidade, estado e data de nascimento são obrigatórios.' })
+    //         }
 
-            if (typeof dados.nome !== "string") {
-                return response
-                    .status(400)
-                    .json({ mensagem: "O nome deve ser uma string" });
-            }
+    //         if (typeof dados.nome !== "string") {
+    //             return response
+    //                 .status(400)
+    //                 .json({ mensagem: "O nome deve ser uma string" });
+    //         }
 
-            const valoresSexo = ['Masculino', 'Feminino', 'Nao especificado'];
-            if (!valoresSexo.includes(dados.sexo)) {
-                return response
-                    .status(400)
-                    .json({ mensagem: 'O valor de sexo é inválido. Os valores permitidos são: \"Masculino\", \"Feminino\", ou \"Nao especificado\".' })
-            }
+    //         const valoresSexo = ['Masculino', 'Feminino', 'Nao especificado'];
+    //         if (!valoresSexo.includes(dados.sexo)) {
+    //             return response
+    //                 .status(400)
+    //                 .json({ mensagem: 'O valor de sexo é inválido. Os valores permitidos são: \"Masculino\", \"Feminino\", ou \"Nao especificado\".' })
+    //         }
 
-            if (!padraoCPF.test(dados.cpf)) {
-                return response
-                    .status(400)
-                    .json({ mensagem: 'O CPF deve conter somente números e ter 11 dígitos.' });
-            }
+    //         if (!padraoCPF.test(dados.cpf)) {
+    //             return response
+    //                 .status(400)
+    //                 .json({ mensagem: 'O CPF deve conter somente números e ter 11 dígitos.' });
+    //         }
 
-            if (padraoDataNascimento.test(dados.dataNascimento) === false) {
-                return response.status(400).json({
-                    mensagem:
-                        "A data de nascimento deve estar no formato YYYY-MM-DD",
-                });
-            }
+    //         if (padraoDataNascimento.test(dados.dataNascimento) === false) {
+    //             return response.status(400).json({
+    //                 mensagem:
+    //                     "A data de nascimento deve estar no formato YYYY-MM-DD",
+    //             });
+    //         }
 
-            if (padraoEmail.test(dados.email) === false) {
-                return response
-                    .status(400)
-                    .json({ mensagem: "O email está no formato inválido" });
-            }
+    //         if (padraoEmail.test(dados.email) === false) {
+    //             return response
+    //                 .status(400)
+    //                 .json({ mensagem: "O email está no formato inválido" });
+    //         }
 
-            if (
-                !(dados.password?.length >= 8 && dados.password?.length <= 16)
-            ) {
-                return response.status(400).json({
-                    mensagem: "A senha deve ter entre 8 e 16 dígitos",
-                });
-            }
+    //         if (
+    //             !(dados.password?.length >= 8 && dados.password?.length <= 16)
+    //         ) {
+    //             return response.status(400).json({
+    //                 mensagem: "A senha deve ter entre 8 e 16 dígitos",
+    //             });
+    //         }
 
-            if (!padraoCEP.test(dados.cep)) {
-                return response
-                    .status(400)
-                    .json({ mensagem: 'O CEP deve conter somente números e ter 8 dígitos.' });
-            }
+    //         if (!padraoCEP.test(dados.cep)) {
+    //             return response
+    //                 .status(400)
+    //                 .json({ mensagem: 'O CEP deve conter somente números e ter 8 dígitos.' });
+    //         }
 
-            const emailExistente = await Usuario.findOne({
-                where: {
-                    email: dados.email,
-                },
-            });
+    //         const emailExistente = await Usuario.findOne({
+    //             where: {
+    //                 email: dados.email,
+    //             },
+    //         });
 
-            if (emailExistente) {
-                return response
-                    .status(409)
-                    .json({ mensagem: "Email já cadastrado" });
-            }
+    //         if (emailExistente) {
+    //             return response
+    //                 .status(409)
+    //                 .json({ mensagem: "Email já cadastrado" });
+    //         }
 
-            const cpfExistente = await Usuario.findOne({
-                where: {
-                    cpf: dados.cpf,
-                },
-            });
+    //         const cpfExistente = await Usuario.findOne({
+    //             where: {
+    //                 cpf: dados.cpf,
+    //             },
+    //         });
 
-            if (cpfExistente) {
-                return response
-                    .status(409)
-                    .json({ mensagem: "CPF já cadastrado" });
-            }
+    //         if (cpfExistente) {
+    //             return response
+    //                 .status(409)
+    //                 .json({ mensagem: "CPF já cadastrado" });
+    //         }
 
-            const usuario = await Usuario.create({
-                nome: dados.nome,
-                sexo: dados.sexo,
-                cpf: dados.cpf,
-                dataNascimento: dados.dataNascimento,
-                email: dados.email,
-                password: dados.password,
-                cep: dados.cep,
-                logradouro: dados.logradouro,
-                bairro: dados.bairro,
-                cidade: dados.cidade,
-                estado: dados.estado,
-                numeroCasa: dados.numeroCasa,
-                complemento: dados.complemento,
-                isOnline: dados.isOnline
-            });
+    //         const usuario = await Usuario.create({
+    //             nome: dados.nome,
+    //             sexo: dados.sexo,
+    //             cpf: dados.cpf,
+    //             dataNascimento: dados.dataNascimento,
+    //             email: dados.email,
+    //             password: dados.password,
+    //             cep: dados.cep,
+    //             logradouro: dados.logradouro,
+    //             bairro: dados.bairro,
+    //             cidade: dados.cidade,
+    //             estado: dados.estado,
+    //             numeroCasa: dados.numeroCasa,
+    //             complemento: dados.complemento,
+    //             isOnline: dados.isOnline
+    //         });
 
-            response.status(201).json({
-                mensagem: "Conta criada com sucesso",
-                nome: usuario.nome,
-                email: usuario.email,
-                createdAt: usuario.createdAt,
-            });
-        } catch (error) {
-            console.log(error)
-            response.status(500).json({
-                mensagem: "Não foi possivel criar a conta: ",
-                error,
-            });
-        }
-    }
+    //         response.status(201).json({
+    //             mensagem: "Conta criada com sucesso",
+    //             nome: usuario.nome,
+    //             email: usuario.email,
+    //             createdAt: usuario.createdAt,
+    //         });
+    //     } catch (error) {
+    //         console.log(error)
+    //         response.status(500).json({
+    //             mensagem: "Não foi possivel criar a conta: ",
+    //             error,
+    //         });
+    //     }
+    // }
 
     async listarUsuarios(request, response) {
         try {
@@ -208,7 +207,7 @@ class UsuarioController {
             usuario.email = dados.email || usuario.email;
 
             if (dados.password) {
-                usuario.password_hash = dados.password;
+                usuario.password = dados.password;
             }
             await usuario.save();
             return response.status(200).json({
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,65 +3,67 @@ const UsuarioController = require("../controllers/UsuarioController");
 
 const usuariosRoutes = new Router();
 
-usuariosRoutes.post(
-    "/",
-    UsuarioController.cadastrarUsuario
-    /* 
-    #swagger.tags = ['Usuário']
-    #swagger.path = '/usuarios'
-    #swagger.method = 'post'
-    #swagger.description = 'Cadastra um novo usuário'
-    #swagger.parameters['obj'] = {
-        in: 'body',
-        description: 'Informe os dados do usuário ou use o exemplo',
-        required: true,
-        schema: { 
-            $nome: 'usuario teste',
-            $sexo: 'Masculino',
-            $cpf: '11122233344',
-            $dataNascimento: '2000-01-01',
-            $email: 'email@email.com',
-            $password: '12345678',
-            $cep: '88047600',
-            $logradouro: 'Logradouro',
-            $bairro: 'Bairro Centro',
-            $cidade: 'Florianópolis',
-            $estado: 'SC',
-            $numeroCasa: '100',
-            $complemento: '1a',
-            $isaOnline: 'false'
-        }
-    }
-    #swagger.responses[201] = {
-        description: 'Usuário criado com sucesso',
-        schema: {
-                message: "Conta criada com sucesso",
-                nome: "nome do usuario",
-                email: "email do usuario",
-                createdAt: "Data de criacao da conta"
-            }
-    }
-    #swagger.responses[400] = { 
-        description: 'Bad Request',
-        schema: {
-                message: "Um o mais dados faltantes. O nome, sexo, cpf, email, password, cep, logradouro, bairro, cidade, estado e data de nascimento são obrigatórios."
-            }
-    }
-    #swagger.responses[409] = {
-        description: 'Conflict',
-        schema: {
-                message: 'Email já cadastrado'
-            } 
-    }        
-    #swagger.responses[500] = {
-        description: 'Internal Server Error',
-        schema: {
-                mensagem: 'Não foi possivel criar a conta'
-            }
-    }                               
-    */
-);
-usuariosRoutes.get("/",UsuarioController.listarUsuarios
+// usuariosRoutes.post(
+//     "/",
+//     UsuarioController.cadastrarUsuario
+//     /* 
+//     #swagger.tags = ['Usuário']
+//     #swagger.path = '/usuarios'
+//     #swagger.method = 'post'
+//     #swagger.description = 'Cadastra um novo usuário'
+//     #swagger.parameters['obj'] = {
+//         in: 'body',
+//         description: 'Informe os dados do usuário ou use o exemplo',
+//         required: true,
+//         schema: { 
+//             $nome: 'usuario teste',
+//             $sexo: 'Masculino',
+//             $cpf: '11122233344',
+//             $dataNascimento: '2000-01-01',
+//             $email: 'email@email.com',
+//             $password: '12345678',
+//             $cep: '88047600',
+//             $logradouro: 'Logradouro',
+//             $bairro: 'Bairro Centro',
+//             $cidade: 'Florianópolis',
+//             $estado: 'SC',
+//             $numeroCasa: '100',
+//             $complemento: '1a',
+//             $isaOnline: 'false'
+//         }
+//     }
+//     #swagger.responses[201] = {
+//         description: 'Usuário criado com sucesso',
+//         schema: {
+//                 message: "Conta criada com sucesso",
+//                 nome: "nome do usuario",
+//                 email: "email do usuario",
+//                 createdAt: "Data de criacao da conta"
+//             }
+//     }
+//     #swagger.responses[400] = { 
+//         description: 'Bad Request',
+//         schema: {
+//                 message: "Um o mais dados faltantes. O nome, sexo, cpf, email, password, cep, logradouro, bairro, cidade, estado e data de nascimento são obrigatórios."
+//             }
+//     }
+//     #swagger.responses[409] = {
+//         description: 'Conflict',
+//         schema: {
+//                 message: 'Email já cadastrado'
+//             } 
+//     }        
+//     #swagger.responses[500] = {
+//         description: 'Internal Server Error',
+//         schema: {
+//                 mensagem: 'Não foi possivel criar a conta'
+//             }
+//     }                               
+//     */
+// );
+
+usuariosRoutes.get("/",
+    UsuarioController.listarUsuarios
     /* 
     #swagger.tags = ['Usuário']
     #swagger.path = '/usuarios'
@@ -91,7 +93,8 @@ usuariosRoutes.get("/",UsuarioController.listarUsuarios
     */
 );
 
-usuariosRoutes.delete("/:id",UsuarioController.deletarUsuarios
+usuariosRoutes.delete("/:id",
+    UsuarioController.deletarUsuarios
     /* 
     #swagger.tags = ['Usuário']
     #swagger.path = '/usuarios/{userID}'
@@ -133,7 +136,8 @@ usuariosRoutes.delete("/:id",UsuarioController.deletarUsuarios
     */
 );
 
-usuariosRoutes.put("/:id",UsuarioController.atualizarUsuarios
+usuariosRoutes.put("/:id",
+    UsuarioController.atualizarUsuarios
     /* 
     #swagger.tags = ['Usuário']
     #swagger.path = '/usuarios/{userID}'
