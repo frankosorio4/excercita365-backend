3af055313b859cbf19bd1b06adb5418f80d4aad9
Frank Osorio <frankosorio4@gmail.com>
2024-11-12 22:18:52 -0300

    FEAT:  updating swagger documentation

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -68,7 +68,7 @@ usuariosRoutes.get("/",
     #swagger.tags = ['Usuário']
     #swagger.path = '/usuarios'
     #swagger.method = 'get'
-    #swagger.description = 'Lista todos os usuários'
+    #swagger.description = 'Lista todos os usuários.'
     #swagger.responses[200] = {
         description: 'OK'        
     }
@@ -99,10 +99,10 @@ usuariosRoutes.put("/",
     #swagger.tags = ['Usuário']
     #swagger.path = '/usuarios'
     #swagger.method = 'put'
-    #swagger.description = 'Atualiza o usuário quando passado o id como params'
+    #swagger.description = 'Atualiza o usuário que esta logado'
     #swagger.parameters['obj'] = {
         in: 'body',
-        description: 'Informe os dados do usuário a modificar ou use o exemplo. Apague os dados que não deseja modificar',
+        description: 'Informe os campos do usuário que deseja modificar. Use o exemplo embaixo e apague os dados que não deseja modificar',
         required: true,
         schema: { 
             $nome: 'usuario modificado',
@@ -139,19 +139,13 @@ usuariosRoutes.put("/",
     */
 );
 
-usuariosRoutes.delete("/:id",
+usuariosRoutes.delete("/",
     UsuarioController.deletarUsuarios
     /* 
     #swagger.tags = ['Usuário']
-    #swagger.path = '/usuarios/{userID}'
+    #swagger.path = '/usuarios'
     #swagger.method = 'delete'
-    #swagger.description = 'Deleta o usuário quando passado o id como params'
-    #swagger.parameters['userID'] = {
-        in: 'path',
-        description: 'ID do usuario',
-        required: true,
-        type: 'integer'
-    }
+    #swagger.description = 'Permite deletar a conta do usuário logado'
     #swagger.responses[204] = {
         description: 'OK'        
     }
