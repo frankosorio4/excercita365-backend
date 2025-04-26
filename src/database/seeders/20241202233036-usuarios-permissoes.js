f45b5d0c66336d74f434aec052f04ac97f0639a3
Frank Osorio <frankosorio4@gmail.com>
2025-04-25 23:12:22 -0300

    try: change from " to ' in migration in tablename

--- src/database/seeders/20241202233036-usuarios-permissoes.js
+++ src/database/seeders/20241202233036-usuarios-permissoes.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert("usuario_permissoes", [
+    await queryInterface.bulkInsert('usuario_permissoes', [
       {
         usuarioId: 1,
         permissaoId: 1,
@@ -14,6 +14,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("usuario_permissoes", null, {});
+    await queryInterface.bulkDelete('usuario_permissoes', null, {});
   },
 };
